a2821ba56f02fb613c7fe4dbc2baed9391e04787
Ruth Fon <ruthfong@college.harvard.edu>
2013-09-07 16:50:12 -0400

    working on sms response to query to scriptdb

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 
-import re
+import re, requests
 
 app = Flask(__name__)
  
+CONFIRM_TYPE = 'confirm'
+AUTOREPLY_TYPE = 'yes'
+OTHER_TYPE = 'other'
+
 @app.route("/", methods=['GET', 'POST'])
 def respond():
     """Respond to incoming calls with a simple text message."""
@@ -17,13 +21,28 @@ def respond():
     body = request.values.get('Body', None)
     
     # See if they are replying to confirm their number
-    regex = re.compile("confirm", re.IGNORECASE)
-    found = regex.findall(body)
-    if len(found) > 0:
+    if regex(CONFIRM_TYPE, body):
         resp.sms("Confirmed!")
+        connectToScriptDb(from_number, CONFIRM_TYPE)
+    elif regex(AUTOREPLY_TYPE, body):
+        resp.sms("Autoreply sent!")
+        connectToScriptDb(from_number, AUTOREPLY_TYPE)
+    elif regex(OTHER_TYPE, body):
+        resp.sms("Custom autoreply sent!")
+        connectToScriptDb(from_number, OTHER_TYPE, body)
     else:
         resp.sms(body)
+    
     return str(resp)
+
+def regex(phrase, body):
+    """Check if a given phrase is contained in a body of text (case insensitive)"""
+    regex = re.compile(phrase, re.IGNORECASE)
+    found = regex.findall(body)
+    return len(found) > 0
+
+def connectToScriptDb(number, type, msg = None):
+    r = requests.get('https://script.google.com/macros/s/AKfycbxvQ_b8bf6gr_6vvIsO5w9B30LJoAm_5G5JrZWykoFw0fCdHBw/exec&type=' + type + '&number=' + number);
 
 if __name__ == "__main__":
     app.run(debug=True)
