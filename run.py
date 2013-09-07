f473dc11df500b5728be22c6a22419a8c6514560
Ruth Fon <ruthfong@college.harvard.edu>
2013-09-07 15:57:13 -0400

    added files to respond to sms messages

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.sms("Hello, Mobile Monkey")
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
+
+'''
 from flask import Flask
 from send_sms import *
 app = Flask(__name__)
@@ -8,3 +25,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
+'''
