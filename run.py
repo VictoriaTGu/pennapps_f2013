7f1c4b21463ecaef22c11d5458a257012d352db9
Ruth Fon <ruthfong@college.harvard.edu>
2013-09-07 16:32:53 -0400

    added confirm logic

--- run.py
+++ run.py
@@ -1,28 +1,29 @@
 from flask import Flask, request, redirect
 import twilio.twiml
- 
+
+import re
+
 app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
+def respond():
     """Respond to incoming calls with a simple text message."""
- 
+
+    # Prepare response
     resp = twilio.twiml.Response()
-    resp.sms("Hello, Mobile Monkey")
+
+    # Get sender's number and the body of their text
+    from_number = request.values.get('From', None)
+    body = request.values.get('Body', None)
+    
+    # See if they are replying to confirm their number
+    regex = re.compile("confirm", re.IGNORECASE)
+    found = regex.findall(body)
+    if len(found) > 0:
+        resp.sms("Confirmed!")
+    else:
+        resp.sms(body)
     return str(resp)
- 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-'''
-from flask import Flask
-from send_sms import *
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
-'''
